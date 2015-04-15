c08409e450be9e7e82933e72a25c9e0e3ad1f2c3
Goku <victorcel@hotmail.com>
2015-04-15 19:46:07 -0300

    corrige erros de linting

--- Aeacus/runner/runner.py
+++ Aeacus/runner/runner.py
@@ -1,14 +1,14 @@
 import subprocess
 import resource
-import sys
+
 
 def set_limits():
     megabyte = 1000*1000
-    resource.setrlimit(resource.RLIMIT_CORE, (10*megabyte,10*megabyte))
+    resource.setrlimit(resource.RLIMIT_CORE, (10*megabyte, 10*megabyte))
     resource.setrlimit(resource.RLIMIT_CPU, (1, 1))
     resource.setrlimit(resource.RLIMIT_FSIZE, (1*megabyte, 1*megabyte))
     resource.setrlimit(resource.RLIMIT_DATA, (100*megabyte, 100*megabyte))
-    resource.setrlimit(resource.RLIMIT_STACK ,(100*megabyte, 100*megabyte))
+    resource.setrlimit(resource.RLIMIT_STACK, (100*megabyte, 100*megabyte))
     resource.setrlimit(resource.RLIMIT_RSS, (100*megabyte, 100*megabyte))
     resource.setrlimit(resource.RLIMIT_NPROC, (0, 0))
     resource.setrlimit(resource.RLIMIT_MEMLOCK, (0, 0))
