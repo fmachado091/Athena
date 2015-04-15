c9872ec34f2e8c8d9066b7a1c12eee414e29d12e
Goku <victorcel@hotmail.com>
2015-04-15 19:36:40 -0300

    Adiciona app Aeacus que sera o juiz

---
+++ Aeacus/runner/runner.py
@@ -0,0 +1,24 @@
+import subprocess
+import resource
+import sys
+
+def set_limits():
+    megabyte = 1000*1000
+    resource.setrlimit(resource.RLIMIT_CORE, (10*megabyte,10*megabyte))
+    resource.setrlimit(resource.RLIMIT_CPU, (1, 1))
+    resource.setrlimit(resource.RLIMIT_FSIZE, (1*megabyte, 1*megabyte))
+    resource.setrlimit(resource.RLIMIT_DATA, (100*megabyte, 100*megabyte))
+    resource.setrlimit(resource.RLIMIT_STACK ,(100*megabyte, 100*megabyte))
+    resource.setrlimit(resource.RLIMIT_RSS, (100*megabyte, 100*megabyte))
+    resource.setrlimit(resource.RLIMIT_NPROC, (0, 0))
+    resource.setrlimit(resource.RLIMIT_MEMLOCK, (0, 0))
+    resource.setrlimit(resource.RLIMIT_AS, (100*megabyte, 100*megabyte))
+
+
+saida = open("saida.txt", "w")
+process = subprocess.Popen(
+    "./programa.out",
+    preexec_fn=set_limits,
+    stderr=subprocess.PIPE,
+    stdout=saida,
+)
