73bad7868dfa06283003eab694c7fdca81e966be
Goku <victorcel@hotmail.com>
2015-05-27 22:57:45 -0300

    passa os arquivos de upload pro servidor

--- Promachos/views.py
+++ Promachos/views.py
@@ -1,10 +1,31 @@
 # from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .forms import UploadFileForm
+from pprint import pprint
+# from Aeacus import judge
 
 
-class HomeView(TemplateView):
+def home(request):
 
-    template_name = "teste_juiz.html"
+    form = UploadFileForm()
+    if request.method == 'POST':
+        entrada = request.FILES.getlist('file')[0]
+        saida = request.FILES.getlist('file')[1]
+        fonte = request.FILES.getlist('file')[2]
+        
+        # resultado = judge.julgar(entrada, saida, fonte)
+
+        return render(
+            request, 'teste_juiz.html',
+            {
+                'form': form,
+                #'resultado':resultado,
+            }
+        )
+
+    return render(request, 'teste_juiz.html', {'form': form})
 
 
 class LoginView(TemplateView):
