85ba425fd0aafeaf948d2acafcea95626a34a16f
Goku <victorcel@hotmail.com>
2015-05-27 23:00:28 -0300

    correcao de pep8

--- Promachos/views.py
+++ Promachos/views.py
@@ -1,9 +1,7 @@
 # from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
 from .forms import UploadFileForm
-from pprint import pprint
 # from Aeacus import judge
 
 
@@ -11,9 +9,9 @@ def home(request):
 
     form = UploadFileForm()
     if request.method == 'POST':
-        entrada = request.FILES.getlist('file')[0]
-        saida = request.FILES.getlist('file')[1]
-        fonte = request.FILES.getlist('file')[2]
+        # entrada = request.FILES.getlist('file')[0]
+        # saida = request.FILES.getlist('file')[1]
+        # fonte = request.FILES.getlist('file')[2]
         
         # resultado = judge.julgar(entrada, saida, fonte)
 
@@ -21,7 +19,7 @@ def home(request):
             request, 'teste_juiz.html',
             {
                 'form': form,
-                #'resultado':resultado,
+                # 'resultado':resultado,
             }
         )
 
