467aa4c668fed72fc077e6931514983dc72b94f2
Goku <victorcel@hotmail.com>
2015-06-10 23:24:45 -0300

    conserta settings

--- Athena/settings.py
+++ Athena/settings.py
@@ -80,7 +80,7 @@ if 'TRAVIS' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE':   'django.db.backends.postgresql_psycopg2',
-            'NAME':     'travisci',
+            'NAME':     'testDB',
             'USER':     'postgres',
             'PASSWORD': '',
             'HOST':     'localhost',
