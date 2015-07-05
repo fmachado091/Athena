e89127e10c952506171f6194395a346355a01c26
César Pereira <cesarpereiradf@hotmail.com>
2015-07-05 03:22:45 -0300

    atualizando o repo

--- Athena/urls.py
+++ Athena/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     url(r'^logout/$', 'Promachos.views.logout'),
     url(r'^cadastro/$', 'Promachos.views.register_user'),
     url(r'^professor/$', 'Promachos.views.professor'),
-    url(r'^prof_ativ/(?P<id_ativ>\w{0,50})/$', 'Promachos.views.prof_ativ'),
+    url(r'^prof_ativ/(?P<id_ativ>[0-9]+)/$', 'Promachos.views.prof_ativ'),
     url(r'^aluno/$', 'Promachos.views.aluno'),
     url(r'^aluno/aluno_ativ/(?P<ativ_id>[0-9]+)/$',
         'Promachos.views.aluno_ativ'),
